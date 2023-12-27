dd9788ee725bc6840cf7a7acddaafc5d6b8c6026
Farhan wundari <fhnw10@gmail.com>
2023-12-27 15:01:24 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
         <Route path="/history" element={<History />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
-        <Route path="/auth/resetPassword/" element={<ResetPaswd />} />
+        <Route path="/auth/resetPassword" element={<ResetPaswd />} />
         <Route path="/detailcourse/:title" element={<DetailCourse />} />
         <Route path="/course" element={<Course />} />
         <Route path="/course/payment/:title" element={<CoursePayment />} />
